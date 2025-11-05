5e2d3bb54c7c983904e4b9e30252ef4491cdafb1
MarcoSilva2007 <marcoladesenvolvedor@gmail.com>
2025-11-05 16:53:48 -0300

    Criação do Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './controller/login-component/login-component.component';
+import { LoginComponent } from './controller/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
