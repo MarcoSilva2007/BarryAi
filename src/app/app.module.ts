bce50c3259af0e6657338dff86bc68dc39cf6cf5
MarcoSilva2007 <marcoladesenvolvedor@gmail.com>
2025-11-18 13:52:54 -0300

    Alterações nas modelagens, app.module e app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponentComponent } from './controller/login-component/login-component.component';
-import { LoginComponent } from './controller/login/login.component';
+import { routes } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponentComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
