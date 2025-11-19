91128262b1b14d2b2e1043d7c10ef99b766ca2b1
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-11-19 14:36:04 -0300

    19.1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
+
 import { HomeComponent } from './home/home.component';
-=======
->>>>>>> fe911e056ce9a0198c2eea2e7c470ac0e0996ce2
+
 
 @NgModule({
   declarations: [
