565d654e413609a9445d86dfe537805806246767
MarcoSilva2007 <marcoladesenvolvedor@gmail.com>
2025-11-19 16:42:36 -0300

    Alterações Majoritarias no Barry (Ainda com erro)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { ChatComponent } from './chat/chat/chat.component'; 
 import { HomeComponent } from './home/home.component';
-import { ChatComponent } from './chat/chat/chat.component';
 
-// As rotas que você definiu
+
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./auth/login/login.component').then((c) => c.LoginComponent),
-  },
-  {
-    path: 'register',
-    loadComponent: () =>
-      import('./auth/register/register.component').then(
-        (c) => c.RegisterComponent
-      ),
-  },
-  {
-    path: 'chat',
-    loadComponent: () =>
-      import('./chat/chat/chat.component').then((c) => c.ChatComponent),
-  },
-  {
-    path: 'admin',
-    loadComponent: () =>
-      import('./admin/admin/admin.component').then((c) => c.AdminComponent),
-  },
-  
-  { path: '**', redirectTo: '/home' },
+
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+
+  { path: 'chat', component: ChatComponent },
+
+  { path: 'home', component: HomeComponent },
+
+  { path: '**', redirectTo: '/chat' }
 ];
 
 @NgModule({
