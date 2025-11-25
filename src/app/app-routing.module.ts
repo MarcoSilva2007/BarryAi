c245c16ad2ad092893c8951c715a7eb6c595a4f3
MarcoSilva2007 <marcoladesenvolvedor@gmail.com>
2025-11-25 09:12:28 -0300

    Atualizações das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat/chat.component'; 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 
 const routes: Routes = [
 
-  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 
   { path: 'chat', component: ChatComponent },
 
   { path: 'home', component: HomeComponent },
 
-  { path: '**', redirectTo: '/chat' }
+  { path: 'login', component: LoginComponent },
+
+  { path: 'register', component: RegisterComponent },
+
+  { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
